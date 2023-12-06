2258b04757ae62b9bcec7fa0c7f20cc45ceb37ba
huelsth <150256186+huelsth@users.noreply.github.com>
2023-12-06 12:06:29 +0100

    Add files via upload
    To figure out how strings work

---
+++ string_examples.py
@@ -0,0 +1,48 @@
+str1 = "Hello, world!"
+str2 = "I love Python."
+str3 = "Hello, world!"
+
+# compare str1 and str2
+print(str1 == str2)
+
+# compare str1 and str3
+print(str1 == str3)
+# create string type variables
+
+name = "Python"
+print(name)
+
+message = "I love Python."
+print(message)
+greet = 'hello'
+
+# access 1st index element
+print(greet[1]) # "e"
+# multiline string 
+message = """
+This test 1
+This is test 2
+"""
+
+print(message)
+greet = "Hello, "
+name = "Thomas"
+
+# using + operator
+result = greet + name
+print(result)
+
+# Output: Hello,Thomas
+greet = 'Hello'
+
+# iterating through greet string
+for letter in greet:
+    print(letter)
+    reet = 'Hello'
+
+greet = 'Hello'
+
+# count length of greet string
+print(len(greet))
+
+# Output: 5
